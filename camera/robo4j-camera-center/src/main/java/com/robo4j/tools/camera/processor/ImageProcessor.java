47257f283a7c20084afd4cc795bd7603b04e0b4b
Miroslav Wengner <miro.wengner@gmail.com>
2017-09-01 12:39:35 +0200

    license update

--- camera/robo4j-camera-center/src/main/java/com/robo4j/tools/camera/processor/ImageProcessor.java
+++ camera/robo4j-camera-center/src/main/java/com/robo4j/tools/camera/processor/ImageProcessor.java
@@ -1,21 +1,18 @@
 /*
- * Copyright (C) 2017. Miroslav Kopecky
- * This ImageProcessor.java  is part of robo4j.
- * path: /Users/miroslavkopecky/GiTHub_MiroKopecky/robo4j-tools/camera/robo4j-camera-center/src/main/java/com/robo4j/tools/camera/processor/ImageProcessor.java
- * module: robo4j-camera-center_main
+ * Copyright (c) 2014, 2017, Marcus Hirt, Miroslav Wengner
  *
- * robo4j is free software: you can redistribute it and/or modify
+ * Robo4J is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * robo4j is distributed in the hope that it will be useful,
+ * Robo4J is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with robo4j .  If not, see <http://www.gnu.org/licenses/>.
+ * along with Robo4J. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.robo4j.tools.camera.processor;
